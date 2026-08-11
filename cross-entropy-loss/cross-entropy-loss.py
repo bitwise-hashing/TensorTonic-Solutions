5e81ac3cfc3a7dbbee6d5110b5d69d6f379469e8
Dhiraj Kumar Sah <115483834+bitwise-hashing@users.noreply.github.com>
2026-08-11 19:00:44 +0530

    Solve cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -7,5 +7,5 @@ def cross_entropy_loss(y_true, y_pred):
     # Write code here
     y_pred = np.array(y_pred)
     y_true = np.array(y_true)
-    return -np.mean(np.log(np.take_along_axis(y_pred, np.expand_dims(y_true, axis=-1),axis=1)))
+    return -np.mean(np.log(np.take_along_axis(y_pred, np.expand_dims(y_true, axis=-1),axis=-1)))
     
