854adfe7e50ce782230e9e984d5c2044ff8100d1
Dhiraj Kumar Sah <115483834+bitwise-hashing@users.noreply.github.com>
2026-08-12 01:00:22 +0530

    Solve info-nce-loss

--- info-nce-loss/info-nce-loss.py
+++ info-nce-loss/info-nce-loss.py
@@ -5,11 +5,11 @@ def info_nce_loss(Z1, Z2, temperature=0.1):
     Compute InfoNCE Loss for contrastive learning.
     """
     # Write code here
-    Z1 = np.array(Z1)
-    Z2 = np.array(Z2)
-    s = (Z1 @ Z2.T)/ temperature
+    z1=np.array(Z1)
+    z2=np.array(Z2)
+    s = z1@z2.T/temperature
     return -np.mean(np.log(softmax(s).diagonal()))
 
-def softmax(X):
-    shifted_x = np.exp(X-np.max(X))
-    return shifted_x/np.sum(shifted_x,axis=-1)
+def softmax(x):
+    exp_x_stable = np.exp(x-np.max(x))
+    return exp_x_stable/np.sum(exp_x_stable,axis=-1)
